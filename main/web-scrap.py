b1d4508c6167898e39f8cf7a8f6588839d9c39ee
ymJung <metalbird0@gmail.com>
2015-11-09 01:26:06 +0900

    [YM-1] comment .

--- main/web-scrap.py
+++ main/web-scrap.py
@@ -13,7 +13,7 @@ driver = webdriver.Firefox()
 # login
 driver.get(login_url)
 driver.find_element_by_id("user_id").send_keys(userId)
-driver.find_element_by_name("password").send_keys(password)
+driver.find_element_by_id("password").send_keys(password)
 form = driver.find_element_by_name("zb_login")
 form.submit()
 
@@ -22,27 +22,29 @@ search = driver.find_element_by_xpath('//*[@id="wrap"]/div[4]/form/div/p/input')
 search.send_keys(value)
 search.send_keys(Keys.ENTER)
 
-pcv_url = driver.find_element_by_link_text('PC버전').get_attribute('href')
-
 links = driver.find_elements_by_class_name('noeffect')
 seq = 0
+
+file = open("test.txt", 'w')
 for link in links:
     linkUrl = link.get_attribute('href')
     driver.get(linkUrl)
-    date = driver.find_element_by_css_selector('#wrap > div.ct > div > h4 > div > span.hi').text.rsplit('|')[-1]
-    writer = driver.find_elements_by_css_selector('#wrap > div.ct > div > h4 > div > span.ct > a').text
+    data = driver.find_element_by_css_selector('#wrap > div.ct > div > h4').text
 
-    driver.get(pcv_url)
-    title = driver.find_element_by_class_name('view_title2').text
-    content = driver.find_element_by_css_selector("#bbs_wrap > div:nth-child(1) > table:nth-child(13) > tbody > tr:nth-child(1) > td > table > tbody > tr > td > table > tbody > tr > td").text
+    title = data.split('\n')[0]
+    writer = data.split('\n')[1]
+    date = data.split('\n')[2].split('|')[2]
+    content = driver.find_element_by_id('KH_Content').text
 
     seq += 1
     data = {"dataId": seq, "title": title, "content": content, "writer": writer, "date": date}
-    print(data)
-    comments = driver.find_elements_by_id('commentContent')
+
+    comments = driver.find_elements_by_id('commentContent')  # do not work
     for comment in comments:
         commentData = comment.text()
         commentWriter = ''
-    comment = {"writer": commentWriter, "date": date, "content": commentData}
-commentDatas = {"dataId": seq, "comment": comment}
+        comment = {"writer": commentWriter, "date": date, "content": commentData}
+        commentDatas = {"dataId": seq, "comment": comment}
+
+file.close()
 driver.close()
