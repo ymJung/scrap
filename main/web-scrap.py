e6cabb4751332b820411f67691a38d2c6ed30c59
ymJung <metalbird0@gmail.com>
2015-11-08 23:45:02 +0900

    [YM-1] comment .

--- main/web-scrap.py
+++ main/web-scrap.py
@@ -1,7 +1,7 @@
 __author__ = 'YoungMin'
 
-login_url = "http://www.ppomppu.co.kr/index.php"
-stock_url = "http://www.ppomppu.co.kr/zboard/zboard.php?id=stock"
+login_url = "http://m.ppomppu.co.kr/new/login.php?s_url=/new/"
+stock_url = "http://m.ppomppu.co.kr/new/bbs_list.php?id=stock"
 userId = ""
 password = ""
 value = "삼익악기"
@@ -18,28 +18,31 @@ form = driver.find_element_by_name("zb_login")
 form.submit()
 
 driver.get(stock_url)
-search = driver.find_element_by_xpath("//*[@id='bbs_wrap']/div/table[6]/tbody/tr[2]/td/table/tbody/tr/td[2]/input")
+search = driver.find_element_by_xpath('//*[@id="wrap"]/div[4]/form/div/p/input')
 search.send_keys(value)
 search.send_keys(Keys.ENTER)
 
-links = driver.find_elements_by_css_selector('#revolution_main_table > tbody > tr > .list_vspace >a')
-dataId = 0
+pcv_url = driver.find_element_by_link_text('PC버전').get_attribute('href')
+
+links = driver.find_elements_by_class_name('noeffect')
+seq = 0
 for link in links:
     linkUrl = link.get_attribute('href')
     driver.get(linkUrl)
-    title = driver.find_element_by_class_name(".view_title").text
-    content = driver.find_element_by_css_selector(
-        "#bbs_wrap > div:nth-child(1) > table:nth-child(13) > tbody > tr:nth-child(1) > td > table > tbody > tr > td > table > tbody > tr > td").text
-    writer = driver.find_element_by_xpath("//*[@id='wrap']/div[4]/div/h4/div/span[1]/a").text
-    date = driver.find_element_by_css_selector("#wrap > div.ct > div > h4 > div > span.hi").text
+    date = driver.find_element_by_css_selector('#wrap > div.ct > div > h4 > div > span.hi').text.rsplit('|')[-1]
+    writer = driver.find_elements_by_css_selector('#wrap > div.ct > div > h4 > div > span.ct > a').text
 
-    dataId += 1
-    data = {"dataId": dataId, "title": title, "content": content, "writer": writer, "date": date}
+    driver.get(pcv_url)
+    title = driver.find_element_by_class_name('view_title2').text
+    content = driver.find_element_by_css_selector("#bbs_wrap > div:nth-child(1) > table:nth-child(13) > tbody > tr:nth-child(1) > td > table > tbody > tr > td > table > tbody > tr > td").text
+
+    seq += 1
+    data = {"dataId": seq, "title": title, "content": content, "writer": writer, "date": date}
     print(data)
-    comments = driver.find_elements_by_id('*commentContent')
+    comments = driver.find_elements_by_id('commentContent')
     for comment in comments:
         commentData = comment.text()
         commentWriter = ''
     comment = {"writer": commentWriter, "date": date, "content": commentData}
-    commentDatas = {"dataId": dataId, "comment": comment}
+commentDatas = {"dataId": seq, "comment": comment}
 driver.close()
