91b9c13200bdb3f2c6199a87c83fd04874384159
ymJung <metalbird0@gmail.com>
2015-11-12 01:44:35 +0900

    [YM-1] comment  parser

--- main/web-scrap.py
+++ main/web-scrap.py
@@ -24,28 +24,39 @@ search.send_keys(value)
 search.send_keys(Keys.ENTER)
 
 links = driver.find_elements_by_class_name('noeffect')
+linkUrlList = []
+for link in links:
+    linkUrlList.append(link.get_attribute('href'))
+
+
 seq = 0
 sep = "|"
-
-for link in links:
-    linkUrl = link.get_attribute('href')
+data = []
+commentData = []
+for linkUrl in linkUrlList:
     driver.get(linkUrl)
-    data = driver.find_element_by_css_selector('#wrap > div.ct > div > h4').text
+    dataHtml = driver.find_element_by_css_selector('#wrap > div.ct > div > h4').text
 
-    title = data.split('\n')[0]
-    writer = data.split('\n')[1]
-    date = data.split('\n')[2].split('|')[2]
+    title = dataHtml.split('\n')[0]
+    writer = dataHtml.split('\n')[1]
+    date = dataHtml.split('\n')[2].split('|')[2]
     content = driver.find_element_by_id('KH_Content').text
 
     seq += 1
-    data = {"dataId": seq, "title": title, "content": content, "writer": writer, "date": date}
+    dataMap = {"dataId": seq, "title": title, "content": content, "writer": writer, "date": date}
+    data.append(dataMap)
 
     comments = driver.find_element_by_css_selector('#wrap > div.ct > div > div.cmAr').text
     comments = comments.split("덧글")
-    for commentData in comments:
-        commentWriter = ''
-        comment = {"writer": commentWriter, "date": date, "content": comment}
-        commentData = {"dataId": seq, "comment": comment}
+    for comment in comments:
+        comment = comment.strip()
+        if comment.find('추천') > 0:
+            commentWriter = comment.split('추천')[0].strip()
+            commentContent = ''.join(comment.split('추천')[1].strip().split('\n')[0:-1])
+            date = comment.split('추천')[1].split('\n')[-1].replace('|', '').strip()
+            commentMap = {"dataId": seq, "writer": commentWriter, "date": date, "content": commentContent}
+            commentData.append(commentMap)
 
+print(data)
+print(commentData)
 driver.close()
-
