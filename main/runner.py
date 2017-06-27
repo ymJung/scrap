37245d0f7f72351cb04f5d667a46a91db9c70518
ymJung <metalbird0@gmail.com>
2017-06-27 19:31:35 +0900

    [YM-MORE-INFO] progress percent

--- main/runner.py
+++ main/runner.py
@@ -551,7 +551,7 @@ class Runner:
         daily_cnt = self.dbm.get_daily_stock_count(date.today())
         forecast_cnt = self.dbm.get_calculated_forecast_count(date.today())
         # {'daily_cnt': daily_cnt, 'forecast_cnt': forecast_cnt}
-        return str(forecast_cnt / daily_cnt * 100) + '%'
+        return 'today percent is : ' + str(forecast_cnt / daily_cnt * 100) + '%'
 
 run = Runner()
 
